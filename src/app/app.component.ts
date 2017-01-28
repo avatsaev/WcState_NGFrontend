12c8b75829c4fe6f4b6f3bf46d1e7f20e26dbf89
A. Vatsaev <azero79@gmail.com>
2017-01-28 18:32:56 +0100

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,63 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {environment} from '../environments/environment'
+declare var Particle:any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
+
+
+export class AppComponent implements OnInit{
   title = 'app works!';
+
+  particle = new Particle();
+
+  wcState = 'occupied';
+
+  accessToken = '';
+
+  ngOnInit(){
+
+    this.particle.login({username: environment.particle_auth.email, password: environment.particle_auth.password}).then(
+        (data) => {
+          console.debug("DATA", data);
+          this.accessToken = data.body.access_token;
+        }
+    ).then(() => {
+
+      this.particle.getVariable({
+        deviceId: '3b003f000247343339373536',
+        name: 'light_state',
+        auth: this.accessToken
+      }).then((data)  => {
+
+        if (data.body.result == 0 ) this.wcState = 'free'
+        else this.wcState = 'occupied'
+      }, function(err) {
+        console.log('An error occurred while getting attrs:', err);
+      });
+
+      this.particle.getEventStream({
+        deviceId: '3b003f000247343339373536',
+        name: 'light_state_changed',
+        auth: this.accessToken
+      }).then( (stream) => {
+        stream.on('event', (data) => {
+          console.log(data.data);
+          if(data.data == "0") this.wcState = 'free';
+          else this.wcState = 'occupied';
+        });
+      });
+
+    });
+
+
+
+
+
+  }
+
+
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,9 @@
 // The list of which env maps to which file can be found in `angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  particle_auth: {
+    email: 'YOUR_PARTICLEIO_EMAIL',
+    password: 'YOUR_PARTICLEIO_PASSWORD'
+  }
 };
